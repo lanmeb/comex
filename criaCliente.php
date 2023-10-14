a71ff612108ef4f365b13f16077a6f87822328bf
Elania <99603894+lanmeb@users.noreply.github.com>
2023-10-14 19:26:52 -0300

    Add files via upload

---
+++ criaCliente.php
@@ -0,0 +1,11 @@
+
+<?php
+$cliente = [
+    'nome' => 'Hermione',
+    'email' => 'mi@email.com',
+    'celular' => '(11) 9999-9999',
+    'endereco' => 'Beco Diagonal'
+];
+foreach ($cliente as $lista) {
+    echo $lista . PHP_EOL;
+ };
