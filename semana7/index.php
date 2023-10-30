2041a60ed6a08bd966cc86c25f471e95993b8059
Elania <99603894+lanmeb@users.noreply.github.com>
2023-10-30 12:37:32 -0300

    Update index.php

--- semana7/index.php
+++ semana7/index.php
@@ -12,38 +12,8 @@ $cliente = new Cliente("Elania", "email@email.com", "(11) 8765-9489", "Rua das F
  echo "Endereço: " . $cliente->getEndereco() . "\n". PHP_EOL;
  echo "Total Compras R$: " . $cliente->getTotalCompras() . "\n". PHP_EOL;
 
-
-//erro ao executar,
-
-
- /*
-//Objeto Produto
- 
- function inserirProduto($produtos, $nome, $preco, $qtdEstoque)
-{
-    $produto = new \Elani\Comex\Produto($nome, $preco, $qtdEstoque);
-    array_push($produtos, $produto);
-    return $produtos;
-}
-
-$produtos = [];
-$produtos = inserirProduto($produtos, 'Harry Potter e a criança amaldiçoada', 99.99, 100);
-
-foreach ($produtos as $produto) {
-    echo "Nome do Produto: " . $produto->getNome() . "\n";
-    echo "Preço: " . $produto->getPreco() . "\n";
-    echo "Quantidade em Estoque: " . $produto->getQtdEstoque() . "\n";
-}
- 
-
-
-/*
-
-
-
 $produto = new Produto ('Harry Potter e a criança amaldiçoada', 99.99, 100);
 echo "Nome do Produto: " . $produto->getNome() . "\n";
 echo "Preço: " . $produto->getPreco() . "\n";
 echo "Quantidade em Estoque: " . $produto->getQtdEstoque() . "\n";
 
-*/
